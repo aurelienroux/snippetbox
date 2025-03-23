e3a8530a1b4288c84447c3b36360e70a8707657a
Aurélien Roux <aureroux13@gmail.com>
2025-03-23 16:06:51 +0100

    add custom logger to server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -60,8 +60,9 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: app.routes(),
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("Starting server", slog.String("addr", *addr))
