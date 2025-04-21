7375176c1d968a1966cec42b279791e77a6dfc9f
Aurélien Roux <aureroux13@gmail.com>
2025-04-21 13:27:30 +0200

    add non field error to validator

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -8,16 +8,16 @@ import (
 )
 
 type Validator struct {
-	FieldErrors map[string]string
+	FieldErrors    map[string]string
+	NonFieldErrors []string
 }
 
 // Valid() returns true if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
-// AddFieldError() adds an error message to the FieldErrors map (so long as no
-// entry already exists for the given key).
+// AddFieldError() adds an error message to the FieldErrors map (so long as no entry already exists for the given key).
 func (v *Validator) AddFieldError(key, message string) {
 	if v.FieldErrors == nil {
 		v.FieldErrors = make(map[string]string)
@@ -26,6 +26,10 @@ func (v *Validator) AddFieldError(key, message string) {
 	if _, exists := v.FieldErrors[key]; !exists {
 		v.FieldErrors[key] = message
 	}
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 // CheckField() adds an error message to the FieldErrors map only if a validation check is not 'ok'.
