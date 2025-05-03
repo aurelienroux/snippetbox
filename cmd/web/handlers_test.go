81f7de7a796af4093cb6a2841b584da2a095ec2e
Aurélien Roux <aureroux13@gmail.com>
2025-05-03 14:51:19 +0200

    refacto as helpers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,59 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/aurelienroux/snippetbox/internal/assert"
 )
 
-// func TestPingLegacy(t *testing.T) {
-// 	rr := httptest.NewRecorder()
-// 	r, err := http.NewRequest(http.MethodGet, "/", nil)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	ping(rr, r)
-
-// 	rs := rr.Result()
-// 	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-// 	defer rs.Body.Close()
-// 	body, err := io.ReadAll(rs.Body)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	body = bytes.TrimSpace(body)
-
-// 	assert.Equal(t, string(body), "OK")
-// }
-
 func TestPing(t *testing.T) {
-	app := &application{
-		logger: slog.New(slog.DiscardHandler),
-	}
+	app := newTestApplication(t)
 
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-
+	assert.Equal(t, code, http.StatusOK)
 	assert.Equal(t, string(body), "OK")
 }
